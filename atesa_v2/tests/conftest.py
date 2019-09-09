c923e5fc7832dcbed8c9f6ed6346dc46a72fa3e0
Tucker Burgin <tburgin@umich.edu>
2019-09-09 09:10:45 -0400

    Deletes conftest.py

--- atesa_v2/tests/conftest.py
+++
@@ -1,6 +0,0 @@
-def test_sessionfinish(session, exitstatus):
-    """ whole test run finishes. """
-    for filename in glob.glob('atesa_v2/tests/test_temp/*'):
-        print('\n')
-        print(sys.path[0] + '/' + filename)
-        os.remove(sys.path[0] + '/' + filename)
