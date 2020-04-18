78aeca74776a8a4527c4a7cae2f81b5501fa895a
Tucker Burgin <tburgin@umich.edu>
2020-04-18 11:38:00 -0400

    Modifes info_err termination to only try to read info_err.out if it had data in it

--- atesa/jobtype.py
+++ atesa/jobtype.py
@@ -510,7 +510,7 @@ class AimlessShooting(JobType):
                     settings.information_error_overdue = True   # so that information error will be checked next time proc_status == 'not_running' regardless of len_data
 
                 # Compare latest information error value to threshold
-                if os.path.exists('info_err.out'):
+                if os.path.exists('info_err.out') and len(open('info_err.out', 'r').readlines()) > 0:
                     last_value = open('info_err.out', 'r').readlines()[-1].split(' ')[1]
                     if float(last_value) <= settings.information_error_threshold:
                         global_terminate = True     # todo: test information error termination criterion
